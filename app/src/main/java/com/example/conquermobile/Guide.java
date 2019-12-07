91394eae536ac334267d5fd4cdf71eaaf78c6ed9
Johnson1320 <hqius13@foxmail.com>
2019-12-07 11:38:37 +0800

    原版

---
+++ app/src/main/java/com/example/conquermobile/Guide.java
@@ -0,0 +1,81 @@
+package com.example.conquermobile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+
+public class Guide extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+    private ViewPager vp;
+    private ViewPagerAdapter vpAdapter;
+    private List<View> views;
+    private ImageView[] dots;
+    private int[] ids = { R.id.iv1, R.id.iv2, R.id.iv3 };
+    private Button start_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guide);
+        initViews();
+        initDots();
+    }
+
+    private void initViews() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        views = new ArrayList<View>();
+        views.add(inflater.inflate(R.layout.one, null));
+        views.add(inflater.inflate(R.layout.two, null));
+        views.add(inflater.inflate(R.layout.three, null));
+
+        vpAdapter = new ViewPagerAdapter(views, this);
+        vp = (ViewPager) findViewById(R.id.viewpager);
+        vp.setAdapter(vpAdapter);
+        start_btn = (Button) views.get(2).findViewById(R.id.start_btn);
+        start_btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(Guide.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        vp.setOnPageChangeListener(this);
+    }
+
+    private void initDots() {
+        dots = new ImageView[views.size()];
+        for (int i = 0; i < views.size(); i++) {
+            dots[i] = (ImageView) findViewById(ids[i]);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {}
+
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {}
+
+    @Override
+    public void onPageSelected(int arg0) {
+        for (int i = 0; i < ids.length; i++) {
+            if (arg0 == i) {
+                dots[i].setImageResource(R.drawable.login_point_selected);
+            } else {
+                dots[i].setImageResource(R.drawable.login_point);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/conquermobile/lib/BackgroundUtil.java
@@ -0,0 +1,103 @@
+package com.example.conquermobile.lib;
+
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by wenmingvs on 2016/1/14.
+ */
+public class BackgroundUtil {
+
+    private static final String TAG = "BackgroundUtil";
+
+    /**
+     * 方法4：通过使用UsageStatsManager获取，此方法是ndroid5.0A之后提供的API
+     * 必须：
+     * 1. 此方法只在android5.0以上有效
+     * 2. AndroidManifest中加入此权限<uses-permission xmlns:tools="http://schemas.android.com/tools" android:name="android.permission.PACKAGE_USAGE_STATS"
+     * tools:ignore="ProtectedPermissions" />
+     * 3. 打开手机设置，点击安全-高级，在有权查看使用情况的应用中，为这个App打上勾
+     *
+     * @param context     上下文参数
+     * @return
+     */
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static String queryUsageStats(Context context) {
+        class RecentUseComparator implements Comparator<UsageStats> {
+            @Override
+            public int compare(UsageStats lhs, UsageStats rhs) {
+                return (lhs.getLastTimeUsed() > rhs.getLastTimeUsed()) ? -1 : (lhs.getLastTimeUsed() == rhs.getLastTimeUsed()) ? 0 : 1;
+            }
+        }
+        RecentUseComparator mRecentComp = new RecentUseComparator();
+        long ts = System.currentTimeMillis();
+        UsageStatsManager mUsageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> usageStats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, ts - 1000 * 10, ts);
+        if (usageStats == null || usageStats.size() == 0) {
+            return "";
+        }
+        Collections.sort(usageStats, mRecentComp);
+        String currentTopPackage = usageStats.get(0).getPackageName();
+        return currentTopPackage;
+    }
+
+    /**
+     * 判断是否有用权限
+     *
+     * @param context 上下文参数
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static boolean HavaPermissionForTest(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+            AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, applicationInfo.uid, applicationInfo.packageName);
+            return (mode == AppOpsManager.MODE_ALLOWED);
+        } catch (PackageManager.NameNotFoundException e) {
+            return true;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static boolean checkAppUsagePermission(Context context) {
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if(usageStatsManager == null) {
+            return false;
+        }
+        long currentTime = System.currentTimeMillis();
+        // try to get app usage state in last 1 min
+        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime - 60 * 1000, currentTime);
+        if (stats.size() == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void requestAppUsagePermission(Context context) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.i(TAG,"Start usage access settings activity fail!");
+        }
+    }
+}
